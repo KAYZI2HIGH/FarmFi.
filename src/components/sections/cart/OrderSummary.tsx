195249b8837cd2240ca2b1783457dac5a0df9ff4
unknown <emmanueladesipe01@gmail.com>
2025-05-23 14:41:43 -0700

    alert of the transaction digest

--- src/components/sections/cart/OrderSummary.tsx
+++ src/components/sections/cart/OrderSummary.tsx
@@ -1,12 +1,79 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+
 
 const OrderSummary = ({ cart }: { cart: Crop[] }) => {
+  
+  // const client = new SuiClient({ url: getFullnodeUrl('devnet') })
+  // const [keypair, setKeypair] = useState<Ed25519Keypair>()
+  const [tx, setTx] = useState()
+
+  // const signSuiTransaction = async () => {
+    // const TxBlock = Transaction.from(fromBase64(tx))
+
+    // const result = await client.signAndExecuteTransaction({
+    //   signer: keypair as Ed25519Keypair,
+    //   transaction: TxBlock,
+    //   options: {
+    //     showEffects: true,
+    //     showEvents: true,
+    //     showObjectChanges: true,
+    //   },
+    // });
+    
+  // const returnedOrderAddress = result;
+  // const wait = await client.waitForTransaction({digest: result.digest})
+  // console.log(wait)
+  // console.log("Returned Order Address:", returnedOrderAddress);
+  // }
+
   const total = cart.reduce(
     (total, item) => total + item.price * item.weight,
     0
   );
+
+  //hardcoding the product
+  const product = {
+    _id: "682fb9eefedd859ffcfbc5b8",
+    price: 3,
+    farmer: "6830a5fd26a731b08230f6a4",
+    suiWalletAddress: "0xcaec2af7f3d0d2227e4b2b0efa47d3f99ccf696f8368a1e577b42fe0a3549f6f",
+  }
+  
+  const handleOrder = async () => {
+    console.log("sending")
+    // let res = await fetch("https://farmfi-node.onrender.com/auth/keypair", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type":"application/json"
+    //   },
+    //   body:JSON.stringify({email:"test1234@gmail.com", password:"test1234"})
+    // })
+    // const data = await res.json()
+
+    // Construct the Ed25519 keypair
+    // const keypair = Ed25519Keypair.fromSecretKey(data.keypair);
+    // setKeypair(keypair)
+
+    const orderres = await fetch("https://farmfi-node.onrender.com/order/create", {
+      method: "POST",
+      headers: {
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify({buyer:"test1234@gmail.com", product, password:"test1234"})
+    })
+
+    const orderdata = await orderres.json()
+    setTx(orderdata.serializedTransaction)
+    console.log(tx)
+    const digest = orderdata.serializedTransaction.digest
+    console.log(digest)
+    alert(digest)
+    // signSuiTransaction()
+  }
+
   return (
     <div className="py-[25px] space-y-4 px-4 bg-[rgba(109,_76,_65,_0.60)] w-full md:max-w-[357px]  text-white overflow-y-auto hide_scrollbar">
       <div className="pb-5  border-b border-white">
@@ -59,8 +126,9 @@ const OrderSummary = ({ cart }: { cart: Crop[] }) => {
       <Button
         className="group bg-[var(--forest-green)] hover:bg-[var(--forest-green)]/90 rounded-full px-6 py-4 call_to_action_btn_text w-full"
         asChild
+        onClick={handleOrder}
       >
-        <Link href={"/"}>Pay now</Link>
+        <Link href={""}>Pay now</Link>
       </Button>
     </div>
   );
